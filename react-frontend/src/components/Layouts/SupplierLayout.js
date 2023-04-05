def6a38c7f46bc234545536d77d246a58d2de903
amirulaimanr <amirulaimanrohaimi@gmail.com>
2023-04-05 23:18:43 +0800

    add SupplierLayout

---
+++ react-frontend/src/components/Layouts/SupplierLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SupplierLayout = (props) => {
+    return <div className="layout-fullwidth-container">{props.children}</div>;
+};
+
+export default SupplierLayout;
