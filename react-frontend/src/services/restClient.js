09a5a3ef1f2eced77147bf2d0fb16bbc73e4258d
amirulaimanr <amirulaimanrohaimi@gmail.com>
2023-04-05 23:20:24 +0800

    add comment to restClient.js

--- react-frontend/src/services/restClient.js
+++ react-frontend/src/services/restClient.js
@@ -1,10 +1,12 @@
+// sets up a client-side FeathersJS application that communicates with a server using REST APIs, and configures it to use the authentication client and the Axios HTTP client library for network requests
+
 import feathers from "@feathersjs/feathers";
 import rest from "@feathersjs/rest-client";
 
 import auth from "@feathersjs/authentication-client";
 import axios from "axios";
 
-if (!process.env.REACT_APP_SERVER_URL) throw `Environmental variable 'REACT_APP_SERVER_URL' is required. Add it to '.env' file. Example: 'REACT_APP_SERVER_URL=http://localhost:3030'.`
+if (!process.env.REACT_APP_SERVER_URL) throw `Environmental variable 'REACT_APP_SERVER_URL' is required. Add it to '.env' file. Example: 'REACT_APP_SERVER_URL=http://localhost:3030'.`;
 const app = feathers();
 const restClient = rest(process.env.REACT_APP_SERVER_URL);
 
