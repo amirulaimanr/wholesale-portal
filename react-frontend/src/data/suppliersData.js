214ea262f1ef94c0229a4cda81f85d5dc4aec7c9
amirulaimanr <amirulaimanrohaimi@gmail.com>
2023-04-27 01:33:17 +0800

    add json data

---
+++ react-frontend/src/data/suppliersData.js
@@ -0,0 +1,52 @@
+const suppliersData = [
+    {
+        name: "Shenzhen N+1 Technology Co. Ltd",
+        type: "Manufacturer",
+        businessType: "Trading Company",
+        employees: 500,
+        logo: "https://s.globalsources.com/IMAGES/SPL/LOGO/897/L8850873897.jpg",
+        products: [
+            { img: "https://p.globalsources.com/IMAGES/PDT/S1188813078/gps-smart-watch-Reloj-Smartwatch.jpg" },
+            { img: "https://p.globalsources.com/IMAGES/PDT/S1197299256/gps-smart-watch-Reloj-Smartwatch.jpg" },
+            { img: "https://p.globalsources.com/IMAGES/PDT/S1185583800/smart-watch-Smart-bracelet-gps-tracker-watch.jpg" },
+        ],
+    },
+    {
+        name: "Shanghai Atlantis Industry Co., Ltd",
+        type: "Manufacturer",
+        businessType: "Agent, Buying",
+        employees: 550,
+        logo: "https://s.globalsources.com/IMAGES/SPL/LOGO/231/L8825616231.jpg",
+        products: [
+            {
+                img: "https://p.globalsources.com/IMAGES/PDT/S1179465743/Smart-induction-stereo-wireless-speaker.jpg",
+            },
+            {
+                img: "https://p.globalsources.com/IMAGES/PDT/S1176084753/Bucket-hat.jpg",
+            },
+            {
+                img: "https://p.globalsources.com/IMAGES/PDT/S1178045599/Mini-portable-electric-shaver.jpg",
+            },
+        ],
+    },
+    {
+        name: "Clover Industrial Co.,Ltd",
+        type: "Manufacturer",
+        businessType: "Exporter",
+        employees: 150,
+        logo: "https://s.globalsources.com/IMAGES/SPL/LOGO/592/L8851616592.jpg",
+        products: [
+            {
+                img: "https://p.globalsources.com/IMAGES/PDT/S1188395048/laptop.jpg",
+            },
+            {
+                img: "https://p.globalsources.com/IMAGES/PDT/S1188395138/laptops.jpg",
+            },
+            {
+                img: "https://p.globalsources.com/IMAGES/PDT/S1188396067/laptop.jpg",
+            },
+        ],
+    },
+];
+
+export default suppliersData;
